b471e2676434495dae4fdf2302b7c6c4a4a0bad2
Sergio Garnica <sgarnica1902@gmail.com>
2022-02-25 16:17:53 -0600

    JWT added to api

--- athletes/urls.py
+++ athletes/urls.py
@@ -1,8 +1,11 @@
 from cgitb import lookup
 from email.mime import base
 from django.urls import path, include
-from .views import PlanView, ScheduleView, AthleteView, PaymentView, PlanAthletesView
+from .views import PlanView, ScheduleView, AthleteView, PaymentView, PlanAthletesView, MyTokenObtainPairView
 from rest_framework_nested import routers
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 
@@ -17,5 +20,7 @@ router.register('payments', PaymentView)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(plan_router.urls))
+    path('', include(plan_router.urls)),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- athletes/views.py
+++ athletes/views.py
@@ -1,29 +1,55 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PlanSerializer, ScheduleSerializer, AthleteSerializer, PaymentSerializer
 from .models import Plan, Schedule, Athlete, Payment
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['first_name'] = user.first_name
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 class PlanView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Plan.objects.all().order_by('id')
     serializer_class = PlanSerializer
 
 
 class ScheduleView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Schedule.objects.all().order_by('hour')
     serializer_class = ScheduleSerializer
 
 
 class AthleteView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Athlete.objects.all().order_by('-id')
     serializer_class = AthleteSerializer
 
 
 class PaymentView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Payment.objects.all().order_by('-id')
     serializer_class = PaymentSerializer
 
 
 class PlanAthletesView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = AthleteSerializer
 
     def get_queryset(self):
